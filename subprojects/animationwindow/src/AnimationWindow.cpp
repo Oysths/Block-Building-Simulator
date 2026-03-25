b3457ff68a235b841e2ef5a99b189c6fc3fc9d77
Ludvig Sørensen <ludvigso@stud.ntnu.no>
2026-03-25 12:02:15 +0100

    noe automatiske endringer

--- subprojects/animationwindow/src/AnimationWindow.cpp
+++ subprojects/animationwindow/src/AnimationWindow.cpp
@@ -44,6 +44,8 @@ TDT4102::AnimationWindow::AnimationWindow(int x, int y, int width, int height, c
     SDL_SetRenderDrawColor(rendererHandle, backgroundColor.redChannel, backgroundColor.greenChannel, backgroundColor.blueChannel, backgroundColor.alphaChannel);
     SDL_RenderClear(rendererHandle);
 
+    SDL_SetRenderDrawBlendMode(rendererHandle, SDL_BLENDMODE_BLEND);
+
     SDL_RendererInfo rendererInfo;
     SDL_GetRendererInfo(rendererHandle, &rendererInfo);
 
@@ -257,6 +259,21 @@ void TDT4102::AnimationWindow::draw_image(TDT4102::Point topLeftPoint, TDT4102::
     image.draw(rendererHandle, topLeftPoint, imageWidth, imageHeight, rotationAngleDegrees, rotationOrigin);
 }
 
+void TDT4102::AnimationWindow::draw_image_region(TDT4102::Point topLeftPoint, TDT4102::Image& image, int imageWidth, int imageHeight, TDT4102::Point sourceTopLeftPoint,  int sourceWidth, int sourceHeight, TDT4102::FlipImage flip) {
+    SDL_RendererFlip sdl_flip = SDL_FLIP_NONE;
+    switch (flip){
+        case TDT4102::FlipImage::HORIZONTAL:
+            sdl_flip = SDL_FLIP_HORIZONTAL;
+            break;
+        case TDT4102::FlipImage::VERTICAL:
+            sdl_flip = SDL_FLIP_VERTICAL;
+            break;
+        default:
+            sdl_flip = SDL_FLIP_NONE;
+    }
+    image.drawRegion(rendererHandle, topLeftPoint, imageWidth, imageHeight, sourceTopLeftPoint, sourceWidth, sourceHeight, sdl_flip);
+}
+
 void TDT4102::AnimationWindow::draw_text(TDT4102::Point topLeftPoint, std::string textToShow, TDT4102::Color color, unsigned int fontSize, TDT4102::Font font) {
     textWindowCounter++;
     std::stringstream windowName;
@@ -382,13 +399,14 @@ void TDT4102::AnimationWindow::startNuklearDraw(TDT4102::Point location, std::st
     // Compute a rectangle that spans the entire size of the window
     // Some padding is needed to accomplish this.
     const unsigned int drawAreaPadding = 20;
+    const unsigned int windowPaddingX = 2 * context->style.window.padding.x;
 
     // If no draw size was specified, use as much space as available inside the window
     struct nk_rect drawAreaSize;
     if (width == 0 && height == 0) {
         drawAreaSize = nk_rect(float(location.x), float(location.y), float(windowSize.x - location.x + drawAreaPadding), float(windowSize.y - location.y + drawAreaPadding));
     } else {
-        drawAreaSize = nk_rect(float(location.x), float(location.y), float(width), float(height));
+        drawAreaSize = nk_rect(float(location.x), float(location.y), float(width + windowPaddingX), float(height));
     }
 
     // Ensuring that all GUI elements have a reasonable minimum height
